1e309b5ef2747aba7cb0d50efe4a6fdb883cb172
zq <1619211154@qq.com>
2016-06-16 11:17:37 +0800

    晴天

--- README.md
+++
@@ -1,2 +0,0 @@
-# AndroidClass
-homework
--- zx.java
+++ zx.java
@@ -5,8 +5,8 @@ public class Zx{
 		System.out.println("Hello zhongxin");
 		System.out.println("Hello xinqing");
 
-
-
+		System.out.println("你好，晴天");
+		System.out.println("你若安好，便是晴天");
 
 	}
 }
