802e8c19d8be1b9eb5a0e1364860001b88afc688
zq <1619211154@qq.com>
2016-06-16 11:33:13 +0800

    考试

--- zx.java
+++ zx.java
@@ -1,12 +1,15 @@
 public class Zx{
 
-	public static viod main(String[] args){
+	public static void main(String[] args){
 
 		System.out.println("Hello zhongxin");
 		System.out.println("Hello xinqing");
 
+		System.out.println("koashi");
+		System.out.println("考完回家");
 
-
+		System.out.println("星期天英语三级");
+		System.out.println("星期六办公软件");
 
 	}
 }
